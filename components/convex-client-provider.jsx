4ff75af15e2ab5abd6d5ff822e38e4a16291a823
Shubham Verma <sshubham681@gmail.com>
2025-05-19 11:12:57 +0530

    Added expenses and dashboard

--- components/convex-client-provider.jsx
+++ components/convex-client-provider.jsx
@@ -9,7 +9,9 @@ const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClientProvider({ children }) {
   return (
-    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+    <ConvexProviderWithClerk client={convex}
+      useAuth={useAuth}
+    >
       {children}
     </ConvexProviderWithClerk>
   );
